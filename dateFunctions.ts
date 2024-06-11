913321b6333656d80498293c189e8c18298dc039
Muurman M. (Martijn) <mmu014@asr.nl>
2024-06-11 15:57:25 +0200

    quiz

--- dateFunctions.ts
+++ dateFunctions.ts
@@ -1,16 +1,17 @@
 export function isChristmasDay(date: Date): boolean {
-  
+
 }
 
-export function isLeapYear(year: number): boolean {
+
+export function isLeapYear(year: number): boolean { 
     
 }
 
 export function calculateAge(birthDate: Date,today:Date): number {
-  
+     
 }
 
-export function isGenZ(birthDate: Date): boolean {
-  
+export function isGenZ(birthDate: Date): boolean {    
+   
 }
 
---
+++ toughQuestions.test.ts
@@ -0,0 +1,32 @@
+// vraag 1
+test('wat is het middelste cijfer van dit getal', ()=> {
+    const randomLongnumber = 787897823141236231098012397878978231412362310980123;
+
+});
+
+// vraag 2
+test('tel het aantal ts in het onderstaande woord', ()=>{
+    const woord ='hottentottententententoonstelling';
+    
+    
+    
+});
+
+// vraag 3: wat klopt er niet aan deze functie
+function multiply(a: number, b: number): number {
+    a * b;
+}
+
+// vraag 4
+test('Een slak klimt uit de put. Hij valt elke nacht een stuk terug. Reken uit hoe lang de slak er over doet om uit de put te komen', ()=> {
+    const slakSneldheidOverdag = 5;
+    const slakSneldheidNacht= -2;
+    const putDiepte = 24;
+    
+});
+
+// vraag 5:
+test('wat is het langste woord?', () => {
+    const text = 'welkom op het a.s.r. ai experience ervarings plein. Kun jij het langste woord vinden? Deze zin gaat nog veel verder zodat het erg moeilijk wordt het met de hand te ontdekken. Misschien zit het woord wel helemaal achteraan deze zin. Je hebt echt geen idee en daarom zou ik maar beter een functie gaan schrijven in plaats van dit te lezen!';
+    
+});
